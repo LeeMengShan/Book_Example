5e60a5d5ff57c82cd74e25e6a896a0d432945547
LeeMengShan <91774583+LeeMengShan@users.noreply.github.com>
2021-11-25 01:55:25 +0800

    Create hw1.dart

---
+++ S2C2/hw1.dart
@@ -0,0 +1,22 @@
+/*
+
+Author: Meng-Shan Lee
+
+ToDo:
+定義一個會回傳字串的方法，
+函數名為iAmStringFunction，並帶有字串型別的函數參數 iAmString, 
+之後回傳字串為 雲育鏈+該參數的內容
+
+*/
+
+
+// 回傳「字串」的方法，函數名為iAmStringFunction
+// 帶有字串型別的函數參數 iAmString
+String iAmStringFunction(String iAmString) {
+
+  // 回傳字串為 雲育鏈+該參數的內容
+  String result = "雲育鏈" + iAmString;
+
+  return result;
+
+}

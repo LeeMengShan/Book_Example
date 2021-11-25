9c2a72892b194b3be58be9bad285f736599ea181
LeeMengShan <91774583+LeeMengShan@users.noreply.github.com>
2021-11-26 02:16:17 +0800

    Create hw1.dart

---
+++ S2C4/hw1.dart
@@ -0,0 +1,16 @@
+/*
+
+Author: Meng-Shan Lee
+
+ToDo:
+建立一個整數的List集合，內容為 [ 3,5,6,1,2 ]
+
+*/
+
+void main() {
+
+  // 建立一個整數的List集合
+  // 內容指定為 [ 3,5,6,1,2 ]
+  List<int> thisList = [3, 5, 6, 1, 2];
+  
+}

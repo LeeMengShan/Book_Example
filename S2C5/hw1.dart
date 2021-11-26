b93ae4a8c5ba0cccd1f5a22aee6d3ed8f405bdb2
LeeMengShan <91774583+LeeMengShan@users.noreply.github.com>
2021-11-26 09:34:47 +0800

    Create hw1.dart

---
+++ S2C5/hw1.dart
@@ -0,0 +1,20 @@
+/*
+
+Author: Meng-Shan Lee
+
+ToDo:
+有一個家庭，爸爸是李小鴻，媽媽是秉鴻李，兒子是李小秉，
+請將father、 mother, son作為Map的key，並將內容填入。
+
+*/
+
+void main() {
+  // Map的key是father, mother...，是字串型態
+  // Map的value是名字，也是字串型態
+  Map<String, String> family = {
+    // key: value,
+    'father': '李小鴻',
+    'mother': '秉鴻李',
+    'son': '李小秉'
+  };
+}

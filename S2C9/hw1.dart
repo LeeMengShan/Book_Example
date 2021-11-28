7f699d4303c708bf137ac627e56a2bb4ec2ebf72
LeeMengShan <91774583+LeeMengShan@users.noreply.github.com>
2021-11-28 18:30:58 +0800

    Create hw1.dart

---
+++ S2C9/hw1.dart
@@ -0,0 +1,27 @@
+/*
+
+Author: Meng-Shan Lee
+
+ToDo:
+設計一個主程序，會印出會議開始，
+中間過程透過Future.delayed印出請老何訂購餐點，
+最後在主程序印出會議結束
+
+
+*/
+
+void main() {
+  
+  // 印出會議開始
+  print("會議開始");
+  
+  // 請老何訂購餐點，稍後執行
+  Future.delayed(
+      Duration(seconds: 1),
+      (){print("請老何訂購餐點");}
+  );
+  
+  // 會議結束
+  print("會議結束");
+}
+

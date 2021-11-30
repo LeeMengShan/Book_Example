dee7b7edc4270796fe087595fe431515693915d5
LeeMengShan <91774583+LeeMengShan@users.noreply.github.com>
2021-11-30 12:26:15 +0800

    Update hw1.dart

--- S2C7/hw1.dart
+++ S2C7/hw1.dart
@@ -1,6 +1,7 @@
 /*
 
 Author: Meng-Shan Lee
+https://www.linkedin.com/in/meng-shan-lee-188035227/
 
 ToDo:
 有一個複雜巢狀 json string，
@@ -24,27 +25,26 @@ ToDo:
 import 'dart:convert';
 
 void main() {
-
   // 宣告一個字串型態的變數，內容如題
-  String validNestedJsonObjectString = """[
-    {"jsonString":"hello"},
-    {"jsonNumber": 123},
-    {"jsonArray": [1,2,3,4,5,6]},
-    {"jsonObject":{
+  String validNestedJsonObjectString = """{
+    "jsonString":"hello",
+    "jsonNumber": 123,
+    "jsonArray": [1,2,3,4,5,6],
+    "jsonObject":{
       "stringInJsonObject":"abc",
       "numberInJsonObject":456,
       "arrayInJsonObject":[7,8,9,1,2,3],
       "objectInJsonObject":{"lastStringField":"cxcxc"}
-    }}
-  ]""";
+      }
+    }""";
 
-  // 解析成可操作的List
-  List<dynamic> jsonArray = jsonDecode(validNestedJsonObjectString);
-  // 調度List內其中一個json object，提取索引值為3的欄位，即arrayInJsonObject所在之欄位
-  Map<String, dynamic> jsObject = jsonArray[3];
-
-  // 再提取此Map之值，並將其內容指定給新宣告的Map
-  Map<String, dynamic> jsMap = jsObject["jsonObject"];
+  // 解析成可操作的Array
+  Map<dynamic, dynamic> jsonArray = jsonDecode(validNestedJsonObjectString);
+  
+  // 調度Map內其中一個json object，提取索引值為3的欄位，即arrayInJsonObject所在之欄位
+  Map<dynamic, dynamic> jsObject = jsonArray["jsonObject"];
+  
   // 提出arrayInJsonObject的內容值
-  print(jsMap['arrayInJsonObject']);
+  print(jsObject['arrayInJsonObject']);
+  
 }
